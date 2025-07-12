0fbf831e468bdf756f5ed5bec3a45cf4ca7f6aeb
Karim Osman <60973591+karimosman89@users.noreply.github.com>
2025-07-13 01:00:57 +0200

    Update resume.py

--- resume.py
+++ resume.py
@@ -1,11 +1,9 @@
 import streamlit as st
-
-
-
+from utils import tr 
 
 # PDF Link and Preview Section
-st.subheader("Resume")
-st.write("You can view or download my detailed resume below.")
+st.subheader(tr("RESUME_SUBHEADER"))
+st.write(tr("RESUME_DESCRIPTION"))
 
 # Use a direct link to the PDF file
 pdf_url = "https://drive.google.com/file/d/1czMtC6Ee_OOrj-58NW5uHHzITDiJ16Zu/preview"  
@@ -13,10 +11,8 @@ pdf_url = "https://drive.google.com/file/d/1czMtC6Ee_OOrj-58NW5uHHzITDiJ16Zu/pre
 st.markdown(f'<iframe src="{pdf_url}#view=FitH" width="700" height="700" frameborder="0"></iframe>', unsafe_allow_html=True)
 
 # Display a link for downloading the resume
-st.markdown(f'### [Download]({pdf_url})')
-
-
+st.markdown(f'### [{tr("RESUME_DOWNLOAD_LINK_TEXT")}]({pdf_url})')
 
 # Footer Section
-st.markdown("---")  # Horizontal line for separation
-st.write("© 2024 Karim Osman - Machine Learning Engineer")
+st.markdown("---")  
+st.write(tr("RESUME_FOOTER"))
