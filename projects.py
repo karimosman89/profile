db5a934be25e375c7e3d2422487156006906e64d
Karim Osman <60973591+karimosman89@users.noreply.github.com>
2025-08-11 00:50:39 +0200

    Update projects.py

--- projects.py
+++ projects.py
@@ -410,7 +410,7 @@ for project in nlp_projects:
         </div>
         
         <p style="font-size: 1.1rem; line-height: 1.6; margin-bottom: 1.5rem;">
-            {project[\'description\]}
+            {project[\'description\']}
         </p>
         
         <div class="tech-stack">
